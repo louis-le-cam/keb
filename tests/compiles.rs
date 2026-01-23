d19156d366ad22deb62b5fb66520053add19712d
Louis Le Cam <louislecam25@gmail.com>
2026-01-23 11:16:41 +0100

    Add a test for factorial function with recursion and if then else, and make test actually test the program output

--- tests/compiles.rs
+++ tests/compiles.rs
@@ -1,11 +1,15 @@
+#![feature(random)]
+
 use std::{
+    env::temp_dir,
     io::Write,
     process::{Command, Stdio},
+    random::random,
 };
 
 use keb::{c, semantic, ssa, syntax, token};
 
-fn test_if_compiles(source: &str) {
+fn test_program(source: &str, expected_output: &str) {
     let tokens = token::lex(&source);
     let syntax = syntax::parse(&tokens);
     let (mut semantic, mut types) = semantic::parse(&source, &tokens, &syntax);
@@ -13,8 +17,11 @@ fn test_if_compiles(source: &str) {
     let ssa = ssa::generate(&source, &tokens, &semantic, &mut types);
     let c = c::generate(&types, &ssa);
 
+    let program_path = temp_dir().join(format!("keb-test-c-output-{:0>32x}.c", random::<u128>(..)));
+
     let mut clang = Command::new("clang")
-        .args(["-xc", "-std=c23", "-"])
+        .args(["-xc", "-std=c23", "-", "-o"])
+        .arg(&program_path)
         .stdin(Stdio::piped())
         .spawn()
         .unwrap();
@@ -23,7 +30,15 @@ fn test_if_compiles(source: &str) {
     stdin.write_all(c.as_bytes()).unwrap();
     stdin.flush().unwrap();
 
-    assert!(clang.wait().unwrap().success());
+    assert!(clang.wait_with_output().unwrap().status.success());
+
+    let program = Command::new(&program_path).output().unwrap();
+    assert!(program.status.success());
+
+    let stdout = &String::from_utf8(program.stdout).unwrap();
+    assert_eq!(stdout, expected_output);
+
+    std::fs::remove_file(&program_path).unwrap();
 }
 
 #[test]
@@ -34,7 +49,7 @@ fn addition_function_with_argument_destructuring() {
         let main = () => print add (8, 4);
     "#;
 
-    test_if_compiles(source);
+    test_program(source, "12\n");
 }
 
 #[test]
@@ -47,7 +62,17 @@ fn if_then_else_expression() {
         );
     "#;
 
-    test_if_compiles(source);
+    test_program(source, "11\n");
+}
+
+#[test]
+fn factorial_recursive_if_then_else() {
+    let source = r#"
+        let fact = (x: u32) => if x then (x * (fact (x - 1))) else 1;
+        let main = () => print fact 8
+    "#;
+
+    test_program(source, "40320\n");
 }
 
 // NOTE: Not yet implemented
@@ -62,7 +87,7 @@ fn print_string() {
         );
     "#;
 
-    test_if_compiles(source);
+    test_program(source, "Hello world!\n");
 }
 
 // NOTE: Not yet implemented
@@ -78,5 +103,5 @@ fn factorial_recursive_match() {
         let main = print fact 8;
     "#;
 
-    test_if_compiles(source);
+    test_program(source, "40320\n");
 }
