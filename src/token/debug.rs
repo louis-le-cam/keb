c0c851c416c513820261f6c88b55e6e32aaaa0c6
Louis Le Cam <louislecam25@gmail.com>
2026-01-26 09:50:06 +0100

    Make token debug-print also print comments and every character from the source.

--- src/token/debug.rs
+++ src/token/debug.rs
@@ -2,23 +2,14 @@ use colored::Colorize as _;
 
 use crate::token::{
     TokenKind, Tokens, parse_identifer, parse_string_escape, parse_string_segment, parse_u64,
+    token_length,
 };
 
 pub fn debug(source: &str, tokens: &Tokens) {
-    let mut in_string = false;
+    let mut offset = 0;
     for (token, (i, kind)) in tokens.entries() {
-        if !in_string {
-            for whitespace in source[..i]
-                .chars()
-                .rev()
-                .take_while(|char| char.is_whitespace())
-                .collect::<Vec<char>>()
-                .into_iter()
-                .rev()
-            {
-                print!("{whitespace}");
-            }
-        }
+        print!("{}", source[offset..i].white().italic());
+        offset = i + token_length(source, tokens, token);
 
         let text = match kind {
             TokenKind::EqualGreater => "=>".bright_yellow(),
@@ -52,14 +43,8 @@ pub fn debug(source: &str, tokens: &Tokens) {
             TokenKind::False => "false".bright_purple(),
             TokenKind::True => "true".bright_purple(),
 
-            TokenKind::StringStart => {
-                in_string = true;
-                "\"".bright_yellow().bold()
-            }
-            TokenKind::StringEnd => {
-                in_string = false;
-                "\"".bright_yellow().bold()
-            }
+            TokenKind::StringStart => "\"".bright_yellow().bold(),
+            TokenKind::StringEnd => "\"".bright_yellow().bold(),
             TokenKind::StringSegment => parse_string_segment(source, &tokens.offsets, token)
                 .bright_yellow()
                 .underline(),
@@ -68,14 +53,8 @@ pub fn debug(source: &str, tokens: &Tokens) {
                 .to_string()
                 .bright_yellow()
                 .bold(),
-            TokenKind::InterpolationStart => {
-                in_string = false;
-                "{".bright_yellow().bold()
-            }
-            TokenKind::InterpolationEnd => {
-                in_string = true;
-                "}".bright_yellow().bold()
-            }
+            TokenKind::InterpolationStart => "{".bright_yellow().bold(),
+            TokenKind::InterpolationEnd => "}".bright_yellow().bold(),
         };
 
         print!("{text}");
