6b66cf166c3d3efd3b8fcef77d830f03a2698adf
Louis Le Cam <louislecam25@gmail.com>
2026-06-06 08:46:42 +0200

    Adapt tests to the new module hierarchy

--- tests/compiles.rs
+++ tests/compiles.rs
@@ -7,7 +7,7 @@ use std::{
     random::random,
 };
 
-use keb::{c_codegen, semantic, ssa, syntax, token};
+use keb::{codegen, semantic, ssa, syntax, token};
 
 fn test_program(source: &str, expected_output: &str) {
     let tokens = token::lex(&source);
@@ -15,7 +15,7 @@ fn test_program(source: &str, expected_output: &str) {
     let (mut semantic, mut types) = semantic::parse(&source, &tokens.offsets, &syntax);
     semantic::infer_types(&mut semantic, &mut types);
     let ssa = ssa::generate(&source, &tokens.offsets, &semantic, &mut types);
-    let c = c_codegen::generate(&types, &ssa);
+    let c = codegen::c::generate(&types, &ssa);
 
     let program_path = temp_dir().join(format!("keb-test-c-output-{:0>32x}.c", random::<u128>(..)));
 
